a75a63388f5f6cf492cdb8fb2b20e7b8be8a33f4
Matt McKnett <mmcknett@gmail.com>
2019-09-11 21:36:33 -0700

    Initial commit with translated tests.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Hash Table Practice (JavaScript)
+See the [Ruby version](https://github.com/AdaGold/hash-practice) for details.
+
+## Installation
+This has been tested with node `12.6.0`. To install, run
+
+```
+npm i
+```
+
+after cloning this project.
+
+## Testing
+Run all tests with:
+
+```
+npm test
+```
---
+++ test/exercises.test.js
@@ -0,0 +1,218 @@
+const expect = require('chai').expect;
+
+describe("exercises", function() {
+ describe("grouped_anagrams", function() {
+ it("will return [] for an empty array", function() {
+      // Arrange
+      const list = [];
+
+      // Act-Assert
+      expect(grouped_anagrams(list)).toEqual([]);
+    });
+
+ it("will work for the README example", function() {
+      // Arrange
+      const list = ["eat", "tea", "tan", "ate", "nat", "bat"];
+
+      // Act
+      const answer = grouped_anagrams(list);
+      const expected_answer = [
+        ["ate","eat","tea"],
+        ["nat","tan"],
+        ["bat"]
+      ];
+
+      // Assert
+      answer.foreach((array, index) => {
+        expect(expected_answer).to.eql(array.sort());
+      });
+    });
+
+ it("will work for strings with no anagrams", function() {
+      // Arrange
+      const list = ["eat", "ear", "tar", "pop", "pan", "pap"];
+
+      // Act
+      const answer = grouped_anagrams(list);
+
+      const expected_answer = [
+        ["eat"],
+        ["ear"],
+        ["tar"],
+        ["pop"],
+        ["pan"],
+        ["pap"]
+      ];
+
+      // Assert
+      answer.foreach((array, index) => {
+        expect(expected_answer).to.include(array.sort());
+      });
+    });
+
+ it("will work for strings that are all anagrams", function() {
+      // Arrange
+      const list = ["eat", "tae", "tea", "eta", "aet", "ate"]
+
+      // Act
+      const answer = grouped_anagrams(list);
+      const expected_answer = [
+        [ "aet", "ate", "eat", "eta", "tae", "tea"]
+      ];
+
+      // Assert
+      answer.foreach((array, index) => {
+        expect(expected_answer).to.include(array.sort());
+      });
+    });
+  });
+
+describe.skip("top_k_frequent_elements", function() {
+ it("works with example 1", function() {
+      // Arrange
+      const list = [1,1,1,2,2,3];
+      const k = 2;
+
+      // Act
+      const answer = top_k_frequent_elements(list, k);
+
+      // Assert
+      expect(answer.sort).toEqual([1,2]);
+    });
+
+ it("works with example 2", function() {
+      // Arrange
+      const list = [1];
+      const k = 1;
+
+      // Act
+      const answer = top_k_frequent_elements(list, k);
+
+      // Assert
+      expect(answer.sort).toEqual([1]);
+    });
+
+ it("will return [] for an empty array", function() {
+      // Arrange
+      const list = [];
+      const k = 1;
+
+      // Act
+      const answer = top_k_frequent_elements(list, k);
+
+      // Assert
+      expect(answer.sort).toEqual([]);
+    });
+
+ it("will work for an array with k elements all unique", function() {
+      // Arrange
+      const list = [1, 2, 3];
+      const k = 3;
+
+      // Act
+      const answer = top_k_frequent_elements(list, k);
+
+      // Assert
+      expect(answer.sort).toEqual([1, 2, 3]);
+    });
+
+ it("will work for an array when k is 1 and several elements appear 1 time (HINT Pick the 1st one)", function() {
+      // Arrange
+      const list = [1, 2, 3];
+      const k = 1;
+
+      // Act
+      const answer = top_k_frequent_elements(list, k);
+
+      // Assert
+      expect(answer.sort).toEqual([1]);
+    });
+
+  });
+
+describe.skip("valid sudoku", function() {
+ it("works for the table given in the README", function() {
+      // Arrange
+      const table = [
+        ["5","3",".",".","7",".",".",".","."],
+        ["6",".",".","1","9","5",".",".","."],
+        [".","9","8",".",".",".",".","6","."],
+        ["8",".",".",".","6",".",".",".","3"],
+        ["4",".",".","8",".","3",".",".","1"],
+        ["7",".",".",".","2",".",".",".","6"],
+        [".","6",".",".",".",".","2","8","."],
+        [".",".",".","4","1","9",".",".","5"],
+        [".",".",".",".","8",".",".","7","9"]
+      ];
+
+      // Act
+      const valid = valid_sudoku(table);
+
+      // Assert
+      expect(valid).toEqual(true);
+    });
+
+ it("fails for the table given in the README", function() {
+      // Arrange
+      const table = [
+        ["8","3",".",".","7",".",".",".","."],
+        ["6",".",".","1","9","5",".",".","."],
+        [".","9","8",".",".",".",".","6","."],
+        ["8",".",".",".","6",".",".",".","3"],
+        ["4",".",".","8",".","3",".",".","1"],
+        ["7",".",".",".","2",".",".",".","6"],
+        [".","6",".",".",".",".","2","8","."],
+        [".",".",".","4","1","9",".",".","5"],
+        [".",".",".",".","8",".",".","7","9"]
+      ];
+
+      // Act
+      const valid = valid_sudoku(table);
+
+      // Assert
+      expect(valid).toEqual(false);
+    });
+
+ it("fails for a duplicate number in a sub-box", function() {
+      // Arrange
+      const table = [
+        ["5","3",".",".","7",".",".",".","."],
+        ["6",".","3","1","9","5",".",".","."],
+        [".","9","8",".",".",".",".","6","."],
+        ["8",".",".",".","6",".",".",".","3"],
+        ["4",".",".","8",".","3",".",".","1"],
+        ["7",".",".",".","2",".",".",".","6"],
+        [".","6",".",".",".",".","2","8","."],
+        [".",".",".","4","1","9",".",".","5"],
+        [".",".",".",".","8",".",".","7","9"]
+      ];
+
+      // Act
+      const valid = valid_sudoku(table);
+
+      // Assert
+      expect(valid).toEqual(false);
+    });
+
+ it("fails for a duplicate number in a bottom right sub-box", function() {
+      // Arrange
+      const table = [
+        ["5","3",".",".","7",".",".",".","."],
+        ["6",".","2","1","9","5",".",".","."],
+        [".","9","8",".",".",".",".","6","."],
+        ["8",".",".",".","6",".",".",".","3"],
+        ["4",".",".","8",".","3",".",".","1"],
+        ["7",".",".",".","2",".",".",".","6"],
+        [".","6",".",".",".",".","2","8","."],
+        [".",".",".","4","1","9","8",".","5"],
+        [".",".",".",".","8",".",".","7","9"]
+      ];
+
+      // Act
+      const valid = valid_sudoku(table);
+
+      // Assert
+      expect(valid).toEqual(false);
+    });
+  });
+});
